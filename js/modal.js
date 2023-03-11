3fc19bb50769ac5e8b7a1719db2131c3599ae745
Igor Boiko <igorboiko@gmail.com>
2023-03-12 01:48:34 +0200

    js booking listeners

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn2: document.querySelector('[data-modal-2]'),
+    openModalBtn3: document.querySelector('[data-modal-3]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     body: document.querySelector('body'),
@@ -8,6 +10,8 @@
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
